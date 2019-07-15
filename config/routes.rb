c0773c235da926ec549930b2c2559084f570e81b
neil <nvseejoor@gmail.com>
2019-07-15 00:49:16 -0400

    todo

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,12 @@ Rails.application.routes.draw do
   # resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, except: [:index]
+  resources :observations, only: [:index, :show]
+  resources :systems, only: [:index, :show]
+  resources :planets, only: [:index, :show]
 
-  resources :users, only: [:show] do
-    resources :observations do
-      resources :systems
-      resources :planets
-    end
+  resources :observations, except: [:index, :show] do
+    resources :systems
+    resources :planets
   end
-
-  # resources :observations, only: [:index, :show]
-  # resources :systems, only: [:index, :show]
-  # resources :planets, only: [:index, :show]
 end
--- todo.md
+++ todo.md
@@ -3,3 +3,11 @@ I went ahead and looked through your code and there is a lot of great functional
 # require login
 
 # have show edit update destory only availble to logged in users under users
+
+# user can be found by session so it doesnt need to be a nested resource
+
+#create planets on ob new/create edit/update/
+(on index/show it will link to edit path to create planets/systems)
+(destroy should destroy all planets and systems created for observation)
+
+# implment sessions and security before moving on so you can carrys session hash all over with you so you dont have to keep users/id in params
