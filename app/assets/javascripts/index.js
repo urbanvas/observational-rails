4bdb70909137f970b5ebd87075040c0ef00bb185
vasu <nvseejoor@gmail.com>
2019-08-17 23:50:37 -0400

    working app

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -59,41 +59,11 @@ const clickHandlers = () => {
 			const galaxy = new Galaxy(data);
 			generateObservationsandUsersforGalaxy(data, galaxy);
 			$(location).attr('href', `/app`);
-			$('#show').append(galaxy.generateGalaxyHTML());
 		});
 	});
 };
 
-// const handleObservationAll = () => {
-// 	const observationsHTML = ({ name, user, galaxy }) =>
-// 		`<p>The name of this observation: <a href="#" class="ob">${name}</a></p>
-//      <p>The owner of this observation: <a href="#" class="ob">${user.username}</a></p>
-//      <p>The galaxy is: <a href="#" class="ob">${galaxy.name}</a></p>
-// 	 </br>`;
-
-// 	const generateObservationStr = (observations) => {
-// 		let observationsStr = ``;
-// 		for (const ob of observations) {
-// 			const observation = new Observation(ob);
-// 			observationsStr += observationsHTML(observation);
-// 		}
-// 		return observationsStr;
-// 	};
-
-// 	$.get('/observations.json', (e) => {
-// 		$('#observations').html(generateObservationStr(e));
-// 		///// fix this, stop going for all the events at the same time
-// 		// $('.ob').each((idx, val) => {
-// 		// 	$(this).on('click', () => {
-// 		// 		console.log('click clik');
-// 		// 	});
-// 		// 	console.log(idx, val);
-// 		// });
-// 		////////////////////////////
-// 	});
-// };
 if ($(location).attr('pathname') === '/app') {
 	renderUser();
 }
 clickHandlers();
-// handleObservationAll();
